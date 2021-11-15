416ac83329eba5d379724f32e23f5dc9ceaa3254
galifornia <martinho87@gmail.com>
2021-11-15 13:15:02 +0100

    Initial configuration

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: true,
+  singleQuote: true,
+  backetSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComman: 'none',
+};
+
+module.exports = options;
