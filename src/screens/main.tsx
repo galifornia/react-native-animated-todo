8537c9dedd313c25016088219f936643cfe984d4
galifornia <martinho87@gmail.com>
2021-11-16 13:07:00 +0100

    Add AnimatedCheckbox component

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,21 +1,35 @@
 import * as React from 'react'
 import { Center, Box, Text, VStack, useColorModeValue } from 'native-base'
 import ThemeToggle from '../components/theme-toggle'
+import AnimatedCheckbox from '../components/animated-checkbox'
+import { Pressable } from 'react-native'
 
-const Main = () => (
-  <Center
-    _dark={{ bg: 'blueGray.900' }}
-    _light={{ bg: 'blueGray.50' }}
-    px={4}
-    flex={1}
-  >
-    <VStack space={5} alignItems="center">
-      <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
-        <Text>Hola</Text>
-      </Box>
-      <ThemeToggle />
-    </VStack>
-  </Center>
-)
+const Main = () => {
+  const [checked, setChecked] = React.useState(false)
+  const handlePressCheckbox = React.useCallback(() => {
+    setChecked((prev) => !prev)
+  }, [])
+  return (
+    <Center
+      _dark={{ bg: 'blueGray.900' }}
+      _light={{ bg: 'blueGray.50' }}
+      px={4}
+      flex={1}
+    >
+      <VStack space={5} alignItems="center">
+        <Box w="100px" h="100px">
+          <Pressable onPress={handlePressCheckbox}>
+            <AnimatedCheckbox checked={checked} />
+          </Pressable>
+        </Box>
+
+        <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
+          <Text>Hola</Text>
+        </Box>
+        <ThemeToggle />
+      </VStack>
+    </Center>
+  )
+}
 
 export default Main
